5113e5d1d86acbebeb9e8fa05fbb7534742c830a
yevgeller <yevgeller@gmail.com>
2023-10-14 15:56:10 -0400

    Testing Syllabary

---
+++ SyllabaryQuizGenerator/SyllabaryQuizGenerator.Tests/SyllabaryShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyllabaryQuizGenerator.Tests
+{
+    [TestClass]
+    public class SyllabaryShould
+    {
+        [TestMethod]
+        public void Check_IsKatakana()
+        {
+            //static string hiragana = "あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔ";
+            char[] katakanaSyllables = "アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴ".ToCharArray();
+            //static string translit = "aiueokagakigikugukegekogosazashijisuzusezesozotadachijitsuzutedetodonaninunenohabapahibipifubupuhebepehobopomamimumemoyayuyo";
+            foreach(char k in katakanaSyllables)
+            {
+                Assert.IsTrue(Syllabary.Syllabary.IsKatakana(k.ToString()));
+            }
+
+        }
+        [TestMethod]
+        public void Check_IsHiragana()
+        {
+            char[] hiraganaSyllables = "あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔ".ToCharArray();
+            //char[] katakanaSyllables = "アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴ".ToCharArray();
+            //static string translit = "aiueokagakigikugukegekogosazashijisuzusezesozotadachijitsuzutedetodonaninunenohabapahibipifubupuhebepehobopomamimumemoyayuyo";
+            foreach (char h in hiraganaSyllables)
+            {
+                Assert.IsTrue(Syllabary.Syllabary.IsHiragana(h.ToString()));
+            }
+
+        }
+    }
+}
