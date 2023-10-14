6d60aa8470eb968bc52b914da0df00c9312936aa
yevgeller <yevgeller@gmail.com>
2023-10-14 16:05:33 -0400

    Reworking Syllabary tests

--- SyllabaryQuizGenerator/SyllabaryQuizGenerator.Tests/SyllabaryShould.cs
+++ SyllabaryQuizGenerator/SyllabaryQuizGenerator.Tests/SyllabaryShould.cs
@@ -9,12 +9,19 @@ namespace SyllabaryQuizGenerator.Tests
     [TestClass]
     public class SyllabaryShould
     {
+        char[] katakanaSyllables, hiraganaSyllables;
+        List<string> translit;
+        [TestInitialize] public void Initialize()
+        {
+            katakanaSyllables = "アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴ".ToCharArray();
+            hiraganaSyllables = "あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔ".ToCharArray();
+            translit = "a i u e o ka ga ki gi ku gu ke ge ko go sa za shi ji su zu se ze so zo ta da chi ji tsu zu te de to do na ni nu ne no ha ba pa hi bi pi fu bu pu he be pe ho bo po ma mi mu me mo ya yu yo".Split(' ').ToList<string>();
+
+        }
+
         [TestMethod]
         public void Check_IsKatakana()
         {
-            //static string hiragana = "あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔ";
-            char[] katakanaSyllables = "アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴ".ToCharArray();
-            //static string translit = "aiueokagakigikugukegekogosazashijisuzusezesozotadachijitsuzutedetodonaninunenohabapahibipifubupuhebepehobopomamimumemoyayuyo";
             foreach(char k in katakanaSyllables)
             {
                 Assert.IsTrue(Syllabary.Syllabary.IsKatakana(k.ToString()));
@@ -24,14 +31,19 @@ namespace SyllabaryQuizGenerator.Tests
         [TestMethod]
         public void Check_IsHiragana()
         {
-            char[] hiraganaSyllables = "あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔ".ToCharArray();
-            //char[] katakanaSyllables = "アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴ".ToCharArray();
-            //static string translit = "aiueokagakigikugukegekogosazashijisuzusezesozotadachijitsuzutedetodonaninunenohabapahibipifubupuhebepehobopomamimumemoyayuyo";
             foreach (char h in hiraganaSyllables)
             {
                 Assert.IsTrue(Syllabary.Syllabary.IsHiragana(h.ToString()));
             }
+        }
 
+        [TestMethod]
+        public void Check_IsTransliteration()
+        {
+            foreach(var s in translit)
+            {
+                Assert.IsTrue(Syllabary.Syllabary.IsTransliteration(s));
+            }
         }
     }
 }
