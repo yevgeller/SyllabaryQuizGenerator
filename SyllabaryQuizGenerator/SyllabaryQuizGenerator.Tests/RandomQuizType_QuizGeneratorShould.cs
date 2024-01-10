530d51a4b944bb6b94ee79bce33cb64125694184
yevgeller@gmail.com <yevgeller@gmail.com>
2024-01-09 21:34:40 -0500

    Adding other test classes

---
+++ SyllabaryQuizGenerator/SyllabaryQuizGenerator.Tests/RandomQuizType_QuizGeneratorShould.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyllabaryQuizGenerator.Tests
+{
+    internal class RandomQuizType_QuizGeneratorShould
+    {
+    }
+}
