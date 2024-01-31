99b53132bf88693f797539b382822f1da072e33f
yevgeller@gmail.com <yevgeller@gmail.com>
2024-01-31 09:11:49 -0500

    Fixing VS warnings

--- SyllabaryQuizGenerator/SyllabaryQuizGenerator/Syllabary/SyllabaryCharacter.cs
+++ SyllabaryQuizGenerator/SyllabaryQuizGenerator/Syllabary/SyllabaryCharacter.cs
@@ -8,9 +8,9 @@ namespace SyllabaryQuizGenerator
 {
     public class SyllabaryCharacter
     {
-        public string Hiragana { get; set; } 
-        public string Katakana { get; set; }
-        public string Transliteration { get; set; }
+        public string Hiragana { get; set; } = string.Empty;
+        public string Katakana { get; set; } = string.Empty;
+        public string Transliteration { get; set; } = string.Empty;
         public int UniqueId { get; set; }
         public bool IsDigraph { get; set; } = false;
         public bool IsWithDiacritics { get; set; } = false;
